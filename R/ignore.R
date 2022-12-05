0a2b6a785b9119348cc48d299439058d728ec441
Hadley Wickham <h.wickham@gmail.com>
2022-12-04 19:34:07 -0600

    Initial commit

---
+++ R/ignore.R
@@ -0,0 +1,11 @@
+ignore_unused_imports <- function() {
+  dplyr::summarise
+  forcats::fct_c
+  ggplot2::ggplot
+  lubridate::ymd
+  purrr::map
+  readr::read_csv
+  stringr::str_c
+  tibble::tibble
+  tidyr::spread
+}
